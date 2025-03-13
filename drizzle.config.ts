ea5573faddc3fa1b7fe48f35156053d7c7a0f44f
昭弘かいと <erlanggaibr2@gmail.com>
2025-03-13 17:18:47 +0700

    updated project and database statics

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,15 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/database/schema.ts',
+  schema: [
+    './src/database/schemas/index.ts',
+    './src/database/schemas/addresses.ts',
+    './src/database/schemas/customers.ts',
+    './src/database/schemas/files.ts',
+    './src/database/schemas/products.ts',
+    './src/database/schemas/transactions.ts',
+    './src/database/schemas/users.ts',
+  ],
   dialect: 'mysql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
