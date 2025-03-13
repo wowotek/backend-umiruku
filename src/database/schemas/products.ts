2f7812b96cfbeaddad31f2828fe7f18d003422f0
昭弘かいと <erlanggaibr2@gmail.com>
2025-03-13 17:15:10 +0700

    added types

--- src/database/schemas/products.ts
+++ src/database/schemas/products.ts
@@ -10,6 +10,7 @@ export const product = mysqlTable('product', {
 });
 
 export type TProductPrice = InferSelectModel<typeof productPrice>;
+export type TFullProductPrice = { product: TProduct | null, product_price: TProductPrice | null };
 export const productPrice = mysqlTable('product_price', {
     id: int().autoincrement().primaryKey(),
     product_id: int().references(() => product.id),
@@ -24,6 +25,5 @@ export const productPriceRelations = relations(productPrice, ({ one }) => ({
 export type TDeliveryPlan = InferSelectModel<typeof deliveryPlan>;
 export const deliveryPlan = mysqlTable('delivery_plan', {
     id: int().autoincrement().primaryKey(),
-    name: varchar({ length: 255 }).notNull(),
-    is_enabled: boolean().notNull()
+    name: varchar({ length: 255 }).notNull()
 });
