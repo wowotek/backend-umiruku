e9b78655a98ab38f0901bac84d14e11da0a95a14
昭弘かいと <erlanggaibr2@gmail.com>
2025-03-13 17:16:21 +0700

    added user_types

--- src/database/schemas/users.ts
+++ src/database/schemas/users.ts
@@ -5,6 +5,7 @@ export type TUser = InferSelectModel<typeof users>;
 export type TUserProtected = Omit<TUser, 'password'> & { password?: string };
 export const users = mysqlTable('users', {
     id: int().autoincrement().primaryKey(),
+    user_type: varchar({ length: 64, enum: ['ADMIN', 'CUSTOMER'] }).notNull(),
     username: varchar({ length: 255 }).notNull().unique(),
     password: varchar({ length: 512 }).notNull().unique(),
 });
